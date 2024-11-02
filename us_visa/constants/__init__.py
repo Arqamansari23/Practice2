5b8c9c0980ca19a5ee116665a91a30a817ad6f89
Your Name <you@example.com>
2024-11-02 04:51:30 -0700

    and

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,4 +1,4 @@
-
+# This File defines all the conatants 
 
 # defining data base constansts
 
@@ -7,3 +7,15 @@ DATABASE_NAME = "US_VISA"     # Data base name in mongo db
 COLLECTION_NAME = "visa_data"     #    Collection name is data base 
 
 MONGODB_URL_KEY = "MONGODB_URL"       # Mongo db URL
+
+
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"   # Feature store folder inside artifacts folder   to hold raw data before train test split 
+DATA_INGESTION_INGESTED_DIR: str = "ingested"      # ingested folder inside artifact folder responsible to hold train and test data 
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2    # Train test ratio 
